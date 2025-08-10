8243712db8929553f64f28eb8e40ab64fcf4fd36
data_world. f <ochiengogola53@gmail.com>
2025-08-10 17:09:00 +0300

    Week 2 done

--- PYTHON-WEEK-2-ASSIGNMENT-main/week_2.py
+++ PYTHON-WEEK-2-ASSIGNMENT-main/week_2.py
@@ -1,12 +1,24 @@
-#Creating a list
+# Create an empty list
 my_list = []
-my_list.append([10,20,30,40])
 
+# Append the elements 10, 20, 30, 40
+my_list.extend([10, 20, 30, 40])
+
+# Insert the value 15 at the second position (index 1)
 my_list.insert(1, 15)
-print(my_list)
-#Extend List
-my_list.extend([50,60,70])
-print(my_list)
-#Remove element
-my_list.remove(70)
-print(my_list)
+
+# Extend the list with another list [50, 60, 70]
+my_list.extend([50, 60, 70])
+
+# Remove the last element
+my_list.pop()
+
+# Sort the list in ascending order
+my_list.sort()
+
+# Find and print the index of the value 30
+index_of_30 = my_list.index(30)
+print("Index of 30:", index_of_30)
+
+# Final list
+print("Final list:", my_list)
