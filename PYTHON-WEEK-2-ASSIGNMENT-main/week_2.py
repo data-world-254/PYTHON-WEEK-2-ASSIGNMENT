0410ce191cef78116d6abd404fdbbbbe4f06aab5
data_world. f <ochiengogola53@gmail.com>
2025-08-10 16:53:50 +0300

    Week 2 Assignment completed

---
+++ PYTHON-WEEK-2-ASSIGNMENT-main/week_2.py
@@ -0,0 +1,12 @@
+#Creating a list
+my_list = []
+my_list.append([10,20,30,40])
+
+my_list.insert(1, 15)
+print(my_list)
+#Extend List
+my_list.extend([50,60,70])
+print(my_list)
+#Remove element
+my_list.remove(70)
+print(my_list)
